88c2c695416b383245ceba7ea20e673ea16a901f
Ann-Anyango <annmwangombe@gmail.com>
2023-03-16 20:17:59 +0300

    first commit

--- working.js
+++ working.js
@@ -2,6 +2,97 @@
 //Extract the first four characters from the string below;
 //"beautiful"
 let text = "Beautiful";
-let result = text.substr(1, 4);
+let result = text.substring(1, 4);
 console.log(result);
+
+// Insert the following string at the tenth index of the below variable:
+// "eat"
+// const food = "I did not have appetite today"
+ let c="eat";
+ let  cha="I did not have appetite today";
+ console.log(cha.replace(cha[10],"eat"));
+
+
+// Count how many times the following string appears in the string variable:
+// 1. "the"
+// 2."s"
+// const story= "She sells sea shells at the sea shore"
+let string = "she sells sea shells at the sea shore";
+let She="s";
+let  Count=string.split(She).length;
+console.log(Count);
+;
+// Convert the following strings into the specified format:
+// 1. UpperCase: "CONfidant"
+// 2. LowerCase: "amazing", "beautiFUL"
+// 3. Title case "A busy office"
+let texst="CONfidant";
+let String = texst.toUpperCase();
+console.log(String);
+ let word="amazing"; 
+let Work = text.toLowerCase();
+console.log(word);
+let  TEXST="beatiFUL"; 
+let van = text.toLowerCase();
+console.log(TEXST);
+let words="Abusy office";
+let Word=words.toLocaleLowerCase();
+console.log(words);
+// Using JavaScript, find the following words from the following strings:
+// 1. "market"
+// const string1 = "The lady went to the market with her sister";
+// 2. "season"
+// const string2 = "My favorite season is spring
+// let mean="The lady went to the market with her sister";
+
+
+//  let b = "JavaScript is the world's most misunderstood programming language.";
+// let set=b.indexOf("Javascript");
+// console.log();
+// let w="I love dancing and also singing";
+//   let sen= w.toUpperCase();
+//  console.log(sen);
+//  let r="PLAYIMG AND RELAXING IS A HELTHY ACTION";
+//  let ser=r.toLocaleLowerCase();
+//  console.log(ser);
+// //  string length
+// let l="Kanini is my sister";
+// let sew=l.length;
+// console.log(sew);
+// // // string replace
+// let t="ball call";
+// let rep=t.replace('c','m');
+// console.log(rep)
+// //IndexOf
+// //reternce the index of the first character of the substring in a string( out put(7)
+// let three="i love rain";
+// // console.log(three.indexOf("rain"));
+// // lastIndexOf
+// console.log(three.indexOf("love"));
+// // startWith()
+// // returns true if a string begins with specified  character(s).if 
+// let four ="i love rain";
+// console.log(four.startsWith("i"));
+// // String endsWith
+// // it reterns if its true or false if the string does not end with it
+// let five="i love rain";
+// console.log(five.endsWith("n"))
+// // String includes()
+// // checks if one the string can be found inside another string
+// // let six="i love rain";
+// // console.log(six.includes(some));
+// // String CharacterData()
+// // reterns the character at the spesific index in a string
+
+// let ten="i love rain";
+// console.log(ten.repeat(3));
+
+
+
+
+// //  ARRAYS
+//  let countries=["Kenya","Uganda","Rwanda","Sudan"];
+//  let cuon=countries.length;
+//  console.log(cuon);
+
 
